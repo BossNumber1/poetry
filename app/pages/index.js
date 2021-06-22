7ebdfbff546b91fabed9d35b1d3b9a1075d7dc2d
BossNumber1 <dimaang23@gmail.com>
2021-06-22 15:51:48 +0300

    сделал так, чтобы стихи шли последовательно, без повторений... чтобы аватарки - именно - к стихам подключались.. чтобы всё отлично робило)

--- app/pages/index.js
+++ app/pages/index.js
@@ -20,12 +20,24 @@ export default function Home() {
                     <div>вспоминай наше светлое чувство.</div>`,
         },
     ];
-    const [selectedScriptSection, setSelectedScriptSection] = React.useState(0);
 
-    const [content, setContent] = React.useState(
-        `<div>приветствую, о, милый путник</div><div>нажимаешь на пробел и погружаешься в мир стихов</div>
+    React.useEffect(() => {
+        localStorage.setItem("hram", JSON.stringify(scriptArray));
+        localStorage.setItem("selectedScriptSection", "false");
+        localStorage.setItem(
+            "selectedText",
+            `<div>приветствую, о, милый путник</div><div>нажимаешь на пробел и погружаешься в мир стихов</div>
                 <p>погнаааали! 🤘</p>`
-    );
+        );
+    }, []);
+
+    const [selectedScriptSection, setSelectedScriptSection] =
+        React.useState(false);
+
+    // const [content, setContent] = React.useState(
+    //     `<div>приветствую, о, милый путник</div><div>нажимаешь на пробел и погружаешься в мир стихов</div>
+    //             <p>погнаааали! 🤘</p>`
+    // );
 
     React.useEffect(() => {
         let randomn = [];
@@ -34,14 +46,43 @@ export default function Home() {
             let listener = function (event) {
                 if (
                     event.code == "Space" &&
-                    selectedScriptSection != scriptArray.length + 1
+                    localStorage.getItem("selectedScriptSection")
+                    // <
+                    //     scriptArray.length
+                    // selectedScriptSection < scriptArray.length
                 ) {
                     randomn = [];
-                    randomn.push(
-                        scriptArray[selectedScriptSection]
-                            ? scriptArray[selectedScriptSection].text
-                            : "конец"
-                    );
+
+                    let lux = localStorage.getItem("selectedScriptSection");
+                    // let lex = JSON.parse(localStorage.getItem("hram"));
+
+                    if (lux === "false") {
+                        // if (selectedScriptSection === false) {
+                        // alert("lux=false");
+                        // randomn.push(scriptArray[0].text); ????
+                        localStorage.setItem("selectedScriptSection", "0");
+                    }
+
+                    // if
+
+                    // (lux < scriptArray.length) {
+                    // alert("aeeee");
+                    //  } else if (selectedScriptSection < scriptArray.length) {
+                    // randomn.push(scriptArray[selectedScriptSection].text);
+                    else if (lux === "0") {
+                        localStorage.setItem("selectedScriptSection", "1");
+                        // randomn.push("hhhh");
+                        // randomn.push(scriptArray[0].text);
+                    } else if (lux === "1") {
+                        //   } else if (selectedScriptSection === 1) {
+                        // alert(lex[1].text);
+                        // randomn.push(scriptArray[1].text);
+                        localStorage.removeItem("selectedScriptSection");
+                    }
+                    // }
+                    // else {?????
+                    //     randomn.push("конец");
+                    // }
 
                     func();
                     document.removeEventListener("keydown", listener);
@@ -53,8 +94,15 @@ export default function Home() {
         runOnKeys(() => setPoetry(randomn));
 
         if (poetry !== "") {
-            setContent(poetry);
-            setSelectedScriptSection(+selectedScriptSection + 1);
+            // setContent(poetry);
+
+            if (selectedScriptSection !== false) {
+                setSelectedScriptSection(Number(selectedScriptSection) + 1);
+                localStorage.setItem("selectedText", scriptArray[1].text);
+            } else {
+                setSelectedScriptSection(0);
+                localStorage.setItem("selectedText", scriptArray[0].text);
+            }
         }
     }, [poetry]);
 
@@ -117,25 +165,42 @@ export default function Home() {
                     <div className="blockBelow2">
                         <div style={{ display: "flex", marginLeft: 20 }}>
                             <div style={{ borderRadius: 50 }}>
-                                <Image
-                                    src="/ava.jpg"
-                                    alt="ava"
-                                    width={74}
-                                    height={74}
-                                    style={{ borderRadius: 50 }}
-                                />
+                                {typeof window !== "undefined" &&
+                                    localStorage.getItem(
+                                        "selectedScriptSection"
+                                    ) &&
+                                    Number(
+                                        localStorage.getItem(
+                                            "selectedScriptSection"
+                                        )
+                                    ) < scriptArray.length && (
+                                        <Image
+                                            src="/ava.jpg"
+                                            alt="ava"
+                                            width={74}
+                                            height={74}
+                                            style={{ borderRadius: 50 }}
+                                        />
+                                    )}
                             </div>
-                            <div
-                                style={{
-                                    marginLeft: 30,
-                                    // position: "absolute"
-                                }}
-                                dangerouslySetInnerHTML={{
-                                    __html: content,
-                                }}
-                            />
+                            {typeof window !== "undefined" &&
+                            localStorage.getItem("selectedScriptSection") ? (
+                                <div
+                                    style={{
+                                        marginLeft: 30,
+                                        // position: "absolute"
+                                    }}
+                                    dangerouslySetInnerHTML={{
+                                        __html: localStorage.getItem(
+                                            "selectedText"
+                                        ),
+                                    }}
+                                />
+                            ) : (
+                                "конец"
+                            )}
                         </div>
-                        {selectedScriptSection == 1 && (
+                        {selectedScriptSection === 0 && (
                             <div style={{ marginTop: "34px" }}>
                                 <Image
                                     src="/one.jpg"
