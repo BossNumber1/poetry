7650bdef517c67815dcb1c0533c1a2552e4dea62
BossNumber1 <dimaang23@gmail.com>
2021-07-06 12:05:15 +0300

    сделал так, чтобы, когда конец, картинка не выводилась

--- app/components/PictureForPoetry.jsx
+++ app/components/PictureForPoetry.jsx
@@ -4,19 +4,20 @@ import Image from "next/image";
 export default function PictureForPoetry({ selectedScriptSection }) {
     return (
         <>
-            {(selectedScriptSection === 0 || selectedScriptSection > 0) && (
-                <div style={{ marginTop: "34px" }}>
-                    <Image
-                        src={`/${
-                            typeof window !== "undefined" &&
-                            localStorage.getItem("picture")
-                        }`}
-                        alt="first_picture"
-                        width={500}
-                        height={333}
-                    />
-                </div>
-            )}
+            {(selectedScriptSection === 0 || selectedScriptSection > 0) &&
+                localStorage.getItem("selectedText") !== "конец" && (
+                    <div style={{ marginTop: "34px" }}>
+                        <Image
+                            src={`/${
+                                typeof window !== "undefined" &&
+                                localStorage.getItem("picture")
+                            }`}
+                            alt="first_picture"
+                            width={500}
+                            height={333}
+                        />
+                    </div>
+                )}
         </>
     );
 }
