84136e3d2c910994cb66c64d7c33c67ed5da10b8
BossNumber1 <dimaang23@gmail.com>
2021-07-10 22:09:46 +0300

    сделал форму, что отправляет на сервер фото и данные

--- app/components/VerseAddingForm.jsx
+++ app/components/VerseAddingForm.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { Formik, Form, Field } from "formik";
-// import axios from "axios";
+// import { Formik, Form, Field } from "formik";
+import axios from "axios";
 
-import { useHttpPost } from "./useHttp.hook";
+// import { useHttpPost } from "./useHttp.hook";
 
 const VerseAddingForm = () => {
-    const [showError, setShowError] = React.useState(false);
+    // const [showError, setShowError] = React.useState(false);
 
-    const { requestPost } = useHttpPost();
+    // const { requestPost } = useHttpPost();
 
     React.useEffect(() => {
         document.querySelector("form").addEventListener("keydown", (event) => {
@@ -15,134 +15,215 @@ const VerseAddingForm = () => {
         });
     }, []);
 
-    return (
-        <Formik
-            initialValues={{
-                nameAuthor: "",
-                avatar: "",
-                verse: "",
-                illustration: "",
-            }}
-            validate={(values) => {
-                if (
-                    !values.nameAuthor ||
-                    !values.avatar ||
-                    !values.verse ||
-                    !values.illustration
-                ) {
-                    setShowError(false);
-                }
-            }}
-            onSubmit={(values, { setSubmitting }) => {
-                setTimeout(() => {
-                    if (
-                        values.nameAuthor !== "" &&
-                        values.avatar !== "" &&
-                        values.verse !== "" &&
-                        values.illustration !== ""
-                    ) {
-                        // alert(JSON.stringify(values, null, 2));
+    const blet = (e) => {
+        e.preventDefault();
 
-                        // axios
-                        //     .get(`http://localhost:80/api/createPost/`)
-                        //     .then((response) => {
-                        //         alert(response);
-                        //         // setAllo(response.data);
-                        //     });
+        let bro = [];
 
-                        requestPost(`http://localhost:80/api/createPost/`, {
-                            nameAuthor: values.nameAuthor,
-                            avatar: values.avatar,
-                            verse: values.verse,
-                            illustration: values.illustration,
-                        }).then((response) => {
-                            alert(response);
-                            // showSuccess(true);
-                            // localStorage.setItem("login", formData.login);
-                            // localStorage.setItem("pass", formData.password);
-                            // dispatch(logOffDouble(false));
-                            // const { pathname } = Router;
-                            // if (pathname == "/access") {
-                            //     Router.push("/booksAll");
-                            // }
-                        });
-                    } else {
-                        setShowError("Стоит полностью заполнить форму");
+        const formData = new FormData(document.querySelector("form"));
+        for (var pair of formData.entries()) {
+            bro.push(pair[1]);
+        }
+
+        if (bro.length > 0) {
+            let formData = new FormData();
+            // formData.append("avatar", bro[1]);
+            // formData.append("illustration", bro[3]);
+
+            // let textData = {
+            //     nameAuthor: bro[0],
+            //     verse: bro[2]
+            // }
+
+            formData.append("nameAuthor", bro[0]);
+            formData.append("avatar", bro[1]);
+            formData.append("verse", bro[2]);
+            formData.append("illustration", bro[3]);
+
+            // axios
+            //     .post("http://localhost:80/api/upload", textData)
+            //     .then((res) => alert(res.data));
+
+            axios
+                .post(
+                    "http://localhost:80/api/upload",
+                    formData,
+
+                    {
+                        headers: {
+                            "Content-Type": "multipart/form-data",
+                        },
                     }
+                )
+                .then((res) => alert(res.data));
+        }
+    };
 
-                    setSubmitting(false);
-                }, 400);
-            }}
+    return (
+        <form
+            // ref="uploadForm"
+            // id="uploadForm"
+            action="http://localhost:80/upload"
+            method="post"
+            encType="multipart/form-data"
+            // style={{ textAlign: "center" }}
         >
-            {({ isSubmitting }) => (
-                <div className="postAddForm" id="postAddFormId">
-                    {showError && (
-                        <div style={{ color: "red" }}>{showError}</div>
-                    )}
-                    <Form>
-                        <fieldset>
-                            <div className="mb-3">
-                                <label for="nik" className="form-label">
-                                    Ваш творческий псевдоним
-                                </label>
+            <div style={{ marginBottom: "20px" }}>
+                <b>
+                    <legend>Псевдоним</legend>
+                </b>
+                <input type="text" name="nameAuthor" />
+            </div>
+            <div style={{ marginBottom: "20px" }}>
+                <b>
+                    <legend>Аватарка</legend>
+                </b>
+                <input type="file" name="avatar" />
+            </div>
+            <div style={{ marginBottom: "20px" }}>
+                <b>
+                    <legend>Стих</legend>
+                </b>
+                <input type="text" name="verse" />
+            </div>
+            <div style={{ marginBottom: "20px" }}>
+                <b>
+                    <legend>Иллюстрация</legend>
+                </b>
+                <input type="file" name="illustration" />
+            </div>
 
-                                <Field
-                                    type="text"
-                                    name="nameAuthor"
-                                    className="form-control"
-                                />
-                            </div>
+            <input type="submit" value="Отправить" onClick={blet} />
 
-                            <div className="mb-3">
-                                <label for="avatar" className="form-label">
-                                    Ваш аватар
-                                </label>
-                                <Field
-                                    type="file"
-                                    name="avatar"
-                                    className="form-form-control form-control-sm"
-                                />
-                            </div>
+            {/* <VerseAddingForm /> */}
+        </form>
+        // <Formik
+        //     initialValues={{
+        //         nameAuthor: "",
+        //         avatar: "",
+        //         verse: "",
+        //         illustration: "",
+        //     }}
+        //     validate={(values) => {
+        //         if (
+        //             !values.nameAuthor ||
+        //             !values.avatar ||
+        //             !values.verse ||
+        //             !values.illustration
+        //         ) {
+        //             setShowError(false);
+        //         }
+        //     }}
+        //     onSubmit={(values, { setSubmitting }) => {
+        //         setTimeout(() => {
+        //             if (
+        //                 values.nameAuthor !== "" &&
+        //                 values.avatar !== "" &&
+        //                 values.verse !== "" &&
+        //                 values.illustration !== ""
+        //             ) {
+        //                 // alert(JSON.stringify(values, null, 2));
 
-                            <div className="mb-3">
-                                <label for="verse" className="form-label">
-                                    Стих
-                                </label>
+        //                 // axios
+        //                 //     .get(`http://localhost:80/api/createPost/`)
+        //                 //     .then((response) => {
+        //                 //         alert(response);
+        //                 //         // setAllo(response.data);
+        //                 //     });
 
-                                <Field
-                                    name="verse"
-                                    as="textarea"
-                                    className="form-control"
-                                    rows="1"
-                                />
-                            </div>
+        //                 requestPost(`http://localhost:80/api/createPost/`, {
+        //                     nameAuthor: values.nameAuthor,
+        //                     avatar: values.avatar,
+        //                     verse: values.verse,
+        //                     illustration: values.illustration,
+        //                 }).then((response) => {
+        //                     alert(response);
+        //                     // showSuccess(true);
+        //                     // localStorage.setItem("login", formData.login);
+        //                     // localStorage.setItem("pass", formData.password);
+        //                     // dispatch(logOffDouble(false));
+        //                     // const { pathname } = Router;
+        //                     // if (pathname == "/access") {
+        //                     //     Router.push("/booksAll");
+        //                     // }
+        //                 });
+        //             } else {
+        //                 setShowError("Стоит полностью заполнить форму");
+        //             }
 
-                            <div className="mb-3">
-                                <label
-                                    for="illustration"
-                                    className="form-label"
-                                >
-                                    Иллюстрация
-                                </label>
-                                <Field
-                                    type="file"
-                                    name="illustration"
-                                    className="form-control form-control-sm"
-                                />
-                            </div>
+        //             setSubmitting(false);
+        //         }, 400);
+        //     }}
+        // >
+        //     {({ isSubmitting }) => (
+        //         <div className="postAddForm" id="postAddFormId">
+        //             {showError && (
+        //                 <div style={{ color: "red" }}>{showError}</div>
+        //             )}
+        //             <Form>
+        //                 <fieldset>
+        //                     <div className="mb-3">
+        //                         <label for="nik" className="form-label">
+        //                             Ваш творческий псевдоним
+        //                         </label>
 
-                            <button
-                                type="submit"
-                                disabled={isSubmitting}
-                                className="btn btn-warning"
-                            >
-                                Сделать частью истории
-                            </button>
-                        </fieldset>
-                    </Form>
-                </div>
-            )}
-        </Formik>
+        //                         <Field
+        //                             type="text"
+        //                             name="nameAuthor"
+        //                             className="form-control"
+        //                         />
+        //                     </div>
+
+        //                     <div className="mb-3">
+        //                         <label for="avatar" className="form-label">
+        //                             Ваш аватар
+        //                         </label>
+        //                         <Field
+        //                             type="file"
+        //                             name="avatar"
+        //                             className="form-form-control form-control-sm"
+        //                         />
+        //                     </div>
+
+        //                     <div className="mb-3">
+        //                         <label for="verse" className="form-label">
+        //                             Стих
+        //                         </label>
+
+        //                         <Field
+        //                             name="verse"
+        //                             as="textarea"
+        //                             className="form-control"
+        //                             rows="1"
+        //                         />
+        //                     </div>
+
+        //                     <div className="mb-3">
+        //                         <label
+        //                             for="illustration"
+        //                             className="form-label"
+        //                         >
+        //                             Иллюстрация
+        //                         </label>
+        //                         <Field
+        //                             type="file"
+        //                             name="illustration"
+        //                             className="form-control form-control-sm"
+        //                         />
+        //                     </div>
+
+        //                     <button
+        //                         type="submit"
+        //                         disabled={isSubmitting}
+        //                         className="btn btn-warning"
+        //                     >
+        //                         Сделать частью истории
+        //                     </button>
+        //                 </fieldset>
+        //             </Form>
+        //         </div>
+        //     )}
+        // </Formik>
     );
 };
 
