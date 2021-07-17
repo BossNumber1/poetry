c4d0a6218d20d88a67add2a7adc13e34665f2b31
BossNumber1 <dimaang23@gmail.com>
2021-07-17 09:40:45 +0300

    вынес ленту в отдельный компонент

---
+++ app/components/Tape.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+
+function Tape() {
+    return (
+        <div className="tape">
+            <div>
+                <div style={{ marginLeft: 83 }}>
+                    <img src="https://clck.ru/WCBTB" height="60px" />
+                </div>
+                <p style={{ fontWeight: "bold", marginLeft: 83 }}>Удобство</p>
+                <div
+                    style={{
+                        position: "fixed",
+                        bottom: 97,
+                        // color: "red",
+                        left: 35,
+                    }}
+                >
+                    Интуитивный интерфейс
+                </div>
+            </div>
+            <div>
+                <div style={{ marginLeft: 83 }}>
+                    <img src="https://clck.ru/WCBpk" height="60px" />
+                </div>
+                <p style={{ fontWeight: "bold", marginLeft: 83 }}>Красота</p>
+                <div
+                    style={{
+                        position: "fixed",
+                        bottom: 97,
+                        left: 260,
+                        // color: "red",
+                    }}
+                >
+                    Красивый дизайн
+                </div>
+            </div>
+            <div>
+                <div style={{ marginLeft: 83 }}>
+                    <img src="https://clck.ru/WCBxa" height="60px" />
+                </div>
+                <p style={{ fontWeight: "bold", marginLeft: 83 }}>Простота</p>
+                <div
+                    style={{
+                        position: "fixed",
+                        bottom: 97,
+                        // color: "red",
+                    }}
+                >
+                    Ничего лишнего, всё по делу
+                </div>
+            </div>
+            <div>
+                <div style={{ marginLeft: 83 }}>
+                    <img src="https://clck.ru/WCBwF" height="60px" />
+                </div>
+                <p style={{ fontWeight: "bold", marginLeft: 63 }}>
+                    Нет рекламы
+                </p>
+                <div
+                    style={{
+                        position: "fixed",
+                        bottom: 97,
+                        // color: "red",
+                        left: 640,
+                    }}
+                >
+                    Чтобы не нарушать идею
+                </div>
+            </div>
+            <div>
+                <div style={{ marginLeft: 83 }}>
+                    <img src="https://clck.ru/WCC7Y" height="60px" />
+                </div>
+                <p style={{ fontWeight: "bold", marginLeft: 83 }}>Новизна</p>
+                <div
+                    style={{
+                        position: "fixed",
+                        bottom: 97,
+                        // color: "red",
+                        left: 840,
+                    }}
+                >
+                    Вы такое встречали? Я - нет
+                </div>
+            </div>
+            <div>
+                <div style={{ marginLeft: 83 }}>
+                    <img src="https://clck.ru/WCC4j" height="60px" />
+                </div>
+                <p style={{ fontWeight: "bold", marginLeft: 83 }}>
+                    Перспективность
+                </p>
+                <div
+                    style={{
+                        position: "fixed",
+                        bottom: 97,
+                        // color: "red",
+                        right: -10,
+                    }}
+                >
+                    У меня ещё полно идей 😉
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Tape;
--- app/pages/index.js
+++ app/pages/index.js
@@ -9,6 +9,8 @@ import Logo from "../components/Logo";
 import Advertising from "../components/Advertising";
 import TopBlock from "../components/topBlock/TopBlock";
 
+import Tape from "../components/Tape.jsx";
+
 export default function Home() {
     const [poetry, setPoetry] = React.useState("");
     const [onMusic, setOnMusic] = React.useState(false);
@@ -291,115 +293,7 @@ export default function Home() {
                     </div>
                     <Logo />
                 </div>
-                <div className="tape">
-                    <div>
-                        <div style={{ marginLeft: 83 }}>
-                            <img src="https://clck.ru/WCBTB" height="60px" />
-                        </div>
-                        <p style={{ fontWeight: "bold", marginLeft: 83 }}>
-                            Удобство
-                        </p>
-                        <div
-                            style={{
-                                position: "fixed",
-                                bottom: 97,
-                                // color: "red",
-                                left: 35,
-                            }}
-                        >
-                            Интуитивный интерфейс
-                        </div>
-                    </div>
-                    <div>
-                        <div style={{ marginLeft: 83 }}>
-                            <img src="https://clck.ru/WCBpk" height="60px" />
-                        </div>
-                        <p style={{ fontWeight: "bold", marginLeft: 83 }}>
-                            Красота
-                        </p>
-                        <div
-                            style={{
-                                position: "fixed",
-                                bottom: 97,
-                                left: 260,
-                                // color: "red",
-                            }}
-                        >
-                            Красивый дизайн
-                        </div>
-                    </div>
-                    <div>
-                        <div style={{ marginLeft: 83 }}>
-                            <img src="https://clck.ru/WCBxa" height="60px" />
-                        </div>
-                        <p style={{ fontWeight: "bold", marginLeft: 83 }}>
-                            Простота
-                        </p>
-                        <div
-                            style={{
-                                position: "fixed",
-                                bottom: 97,
-                                // color: "red",
-                            }}
-                        >
-                            Ничего лишнего, всё по делу
-                        </div>
-                    </div>
-                    <div>
-                        <div style={{ marginLeft: 83 }}>
-                            <img src="https://clck.ru/WCBwF" height="60px" />
-                        </div>
-                        <p style={{ fontWeight: "bold", marginLeft: 63 }}>
-                            Нет рекламы
-                        </p>
-                        <div
-                            style={{
-                                position: "fixed",
-                                bottom: 97,
-                                // color: "red",
-                                left: 640,
-                            }}
-                        >
-                            Чтобы не нарушать идею
-                        </div>
-                    </div>
-                    <div>
-                        <div style={{ marginLeft: 83 }}>
-                            <img src="https://clck.ru/WCC7Y" height="60px" />
-                        </div>
-                        <p style={{ fontWeight: "bold", marginLeft: 83 }}>
-                            Новизна
-                        </p>
-                        <div
-                            style={{
-                                position: "fixed",
-                                bottom: 97,
-                                // color: "red",
-                                left: 840,
-                            }}
-                        >
-                            Вы такое встречали? Я - нет
-                        </div>
-                    </div>
-                    <div>
-                        <div style={{ marginLeft: 83 }}>
-                            <img src="https://clck.ru/WCC4j" height="60px" />
-                        </div>
-                        <p style={{ fontWeight: "bold", marginLeft: 83 }}>
-                            Перспективность
-                        </p>
-                        <div
-                            style={{
-                                position: "fixed",
-                                bottom: 97,
-                                // color: "red",
-                                right: -10,
-                            }}
-                        >
-                            У меня ещё полно идей 😉
-                        </div>
-                    </div>
-                </div>
+                <Tape />
             </main>
             <footer className="signature">
                 © <a href="https://vk.com/moyustimov">Устинов Дмитрий</a>,
