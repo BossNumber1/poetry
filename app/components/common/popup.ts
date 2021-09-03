65285c1df2f56cef50025befffaa0d38a0a42b26
BossNumber1 <dimaang23@gmail.com>
2021-09-03 23:51:57 +0300

    Rewrote the file into a Typescript and changed the value in the tsconfig

--- app/components/common/popup.ts
+++ app/components/common/popup.ts
@@ -1,4 +1,4 @@
-module.exports = function popup(url) {
+module.exports = function popup(url: string) {
     if (typeof window !== "undefined") {
         window.open(url, "", "toolbar=0,status=0,width=626,height=436");
     }
