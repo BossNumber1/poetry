342ce2bec941c9e85a4e6e2146ff04590de19262
BossNumber1 <dimaang23@gmail.com>
2021-09-03 11:05:51 +0300

    Improved signature

--- app/components/blockBelow/BlockBelow.jsx
+++ app/components/blockBelow/BlockBelow.jsx
@@ -82,8 +82,20 @@ function BlockBelow({ versesArray, epilogue, verseNumber }) {
             <div className="fourteenthBlock">
                 {verseNumber === "false" && (
                     <>
-                        ©{"    "}
-                        <a href="https://vk.com/moyustimov">Устинов Дмитрий</a>,
+                        ©
+                        <a
+                            href="https://vk.com/moyustimov"
+                            style={{ margin: "0 5px 0 5px" }}
+                        >
+                            Устинов
+                        </a>
+                        и
+                        <a
+                            href="https://vk.com/dimitron_rock"
+                            style={{ margin: "0 5px 0 5px" }}
+                        >
+                            Рогут,
+                        </a>
                         2021-й год
                     </>
                 )}
