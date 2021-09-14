a3ad695f8341e23b6ff380fca9733be0f3470b53
BossNumber1 <dimaang23@gmail.com>
2021-09-14 23:29:11 +0300

    Added an error page and a redirect from it to the main page

---
+++ app/pages/404.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+export default function Error() {
+    const router = useRouter();
+
+    React.useEffect(() => {
+        router.push("http://localhost:3000/");
+    }, []);
+
+    return (
+        <div className="errorMessage">
+            Oops! Немного не туда попал. Перенаправляю на верный адрес...
+            <style jsx>{`
+                .errorMessage {
+                    position: absolute;
+                    top: 50%;
+                    left: 50%;
+                }
+            `}</style>
+        </div>
+    );
+}
